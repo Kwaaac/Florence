589d1502507fbf4dcefe345587631d0bbc600f8c
Maxime <max.rodriguezidf@gmail.com>
2019-05-30 17:44:40 +0200

    JavaScript transport
    Javascript ajouté pour toujours décoché les checkbox qui permettent de 
    cacher le texte et si il est dérouler de changer la taille de la boite 
    pour un affichage au poil

--- fr/Pages/Sousmenus/transport.js
+++ fr/Pages/Sousmenus/transport.js
@@ -1,6 +1,19 @@
-$(document).ready(function(){
-  $(".accordian").click(function() {
-    $(this).nextAll(".content:first").slideToggle("fast");
-    $(this).find(".accordian-arrow").toggleClass("less");
-  });
-});
+// Les checkbox sont toujours initialisées à décoché
+var checkboxes = document.getElementsByTagName('input');
+
+for (var i=0; i<checkboxes.length; i++)  {
+  if (checkboxes[i].type == 'checkbox')   {
+    checkboxes[i].checked = false;
+  }
+}
+
+// Changer la taille du texte pour le show more / show less
+function myFunction(bil, bon) {
+  var le = document.getElementById(bil);
+  var hobbit = document.getElementById(bon);
+  if (le.checked == true){
+    hobbit.style.height = 'auto';
+  } else {
+     hobbit.style.height = '0';
+  }
+}

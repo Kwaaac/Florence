3ece1bf525d7895d8b7e12d6ebc8d085841fbb47
Schneider <leoschneidermaunoury@gmail.com>
2019-06-04 14:54:30 +0200

    Correction footer page d'accueil
    Margin top du footer + taille du slider changes pour faire rentrer le footer en bas de page

--- fr/index.php
+++ fr/index.php
@@ -10,7 +10,7 @@
     <title>Florence</title>
 </head>
 
-<body>
+<body id="accueil">
   <div class="header">
     <ul class="logo">
       <a href="test.html"><img src="Images/Logo.png" alt="Logo"></a>
@@ -181,14 +181,14 @@
     <footer class="footer">
       <div class="Partenaires">
         <ul>Les partenaires de ce projet
-          <li><a class="ftr" href="https://fr.unesco.org/" >L'UNESCO</a></li>
+          <li><br><a class="ftr" href="https://fr.unesco.org/" >L'UNESCO</a></li>
           <li><a class="ftr" href="http://www.u-pem.fr/">L'Université Paris-Est Marne-La-Vallée</a></li>
         </ul>
       </div>
 
       <div class="Propos">
         <ul>A Propos
-          <li><a class="ftr" href="contact.html">Objectif et créateurs du site</a></li>
+          <li><br><a class="ftr" href="contact.html">Objectif et créateurs du site</a></li>
           <li><a class="ftr" href="politiqueContenu.html">Mentions Légales</a></li>
         </ul>
 

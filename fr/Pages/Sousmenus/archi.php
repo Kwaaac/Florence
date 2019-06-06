cfe8902605955463e6ad89c1f445bdc200933f44
Schneider <leoschneidermaunoury@gmail.com>
2019-06-06 15:20:13 +0200

    Ajout lien sur le titre vers page individuelle du Monument

--- fr/Pages/Sousmenus/archi.php
+++ fr/Pages/Sousmenus/archi.php
@@ -17,87 +17,87 @@
 
     <div class="monuments">
       <a href="architecture/PalazzoVe.php"><img src="../../Images/PaVe/palazzo vecchio.jpg" alt="Palazzo Vecchio"></a>
-      <h3>Palazzo Vecchio</h3>
+      <h3><a class="titreMonument" href="architecture/PallazzoVe.php">Palazzo Vecchio</a></h3>
     </div>
 
     <div class="monuments">
-      <a href="architecture/Duomo.php"><img class="image3" src="../../Images/Duomo/Duomo1.jpg" alt="Duamo"></a>
-      <h3>Duomo</h3>
+      <a href="architecture/Duomo.php"><img class="image3" src="../../Images/Duomo/Duomo1.jpg" alt="Duomo"></a>
+      <h3><a class="titreMonument" href="architecture/Duomo.php">Duomo</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/PonteVe.php"><img class="image3" src="../../Images/PoVe/Ponte_Vecchio2.JPG" alt="Ponte Vecchio"></a>
-      <h3>Ponte Vecchio</h3>
+      <h3><a class="titreMonument" href="architecture/PonteVe.php">Ponte Vecchio</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/Offices.php"><img class="image" src="../../Images/MuOf/Musée2.png" alt="Musée des Offices"></a>
-      <h3>Musée des Offices</h3>
+      <h3><a class="titreMonument" href="architecture/Offices.php">Musée des Offices</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/JardinBoboli.php"><img class="image3" src="../../Images/JaBo/Jardin1.jpg" alt="Jardin Boboli"></a>
-      <h3>Jardin Boboli</h3>
+      <h3><a class="titreMonument" href="architecture/JardinBoboli.php">Jardin Boboli</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/PalaisPitti.php"><img class="image3" src="../../Images/PaPi/Florence-PalaisPitti.jpg" alt="Palais Pitti"></a>
-      <h3>Palais Pitti</h3>
+      <h3><a class="titreMonument" href="architecture/PalaisPitti.php">Palais Pitti</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/Bargello.php"><img class="image3" src="../../Images/Bargello/bargello_inside.jpg" alt="Bargello"></a>
-      <h3>Bargello</h3>
+      <h3><a class="titreMonument" href="architecture/Bargello.php">Bargello</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/BasiliqueSanLorenzo.php"><img class="image3" src="../../Images/SanLorenzo/BasiliqueSanLorenzo.jpg" alt="SanLorenzo"></a>
-      <h3>Basilique San Lorenzo</h3>
+      <h3><a class="titreMonument" href="architecture/BasiliqueSanLorenzo.php">Basilique San Lorenzo</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/BasiliqueSanMiniato.php"><img class="image3" src="../../Images/SanMiniato/SanMiniatoAlMonte.jpg" alt="SanMiniato"></a>
-      <h3>Basilique San Miniato</h3>
+      <h3><a class="titreMonument" href="architecture/BasiliqueSanMiniato.php">Basilique San Miniato</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/SantaCroce.php"><img class="image3" src="../../Images/SantaCroce/image1.jpg" alt="SantaCroce"></a>
-      <h3>Santa Croce</h3>
+      <h3><a class="titreMonument" href="architecture/SantaCroce.php">Santa Croce</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/SantaMariaNovella.php"><img class="image3" src="../../Images/SantaMariaNovella/santa-maria-novella.jpg" alt="SantaMariaNovella"></a>
-      <h3>Santa Maria Novella</h3>
+      <h3><a class="titreMonument" href="architecture/SantaMariaNovella.php">Santa Maria Novella</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/PlaceSignoria.php"><img class="image3" src="../../Images/PlaceSignoria/Piazza_Signoria.jpg" alt="PlaceSignoria"></a>
-      <h3>Piazza della Signoria</h3>
+      <h3><a class="titreMonument" href="architecture/PlaceSignoria.php">Piazza della Signoria</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/ForteresseBasso.php"><img class="image3" src="../../Images/ForteresseBasso/Fortezza_da_Basso_1.JPG" alt="ForteresseBasso"></a>
-      <h3>Forteresse de Basso</h3>
+      <h3><a class="titreMonument" href="architecture/ForteresseBasso.php">Forteresse de Basso</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/CampanileGiotto.php"><img class="image3" src="../../Images/CampanileGiotto/Campaniledigiotto.JPG" alt="CampanileGiotto"></a>
-      <h3>Campanile de Giotto</h3>
+      <h3><a class="titreMonument" href="architecture/CampanileGiotto.php">Campanile de Giotto</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/GalleriadellAccademia.php"><img class="image3" src="../../Images/GalleriaAccademia/galleria.jpg" alt="GalleriaAccademia"></a>
-      <h3>Galleria dell'Accademia</h3>
+      <h3><a class="titreMonument" href="architecture/GalleriadellAccademia.php">Galleria dell'Accademia</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/GiardinoBardini.php"><img class="image3" src="../../Images/GiardinoBardini/1.jpg" alt="GiardinoBardini"></a>
-      <h3>Giardino Bardini</h3>
+      <h3><a class="titreMonument" href="architecture/GiardinoBardini.php">Giardino Bardini</a></h3>
     </div>
 
     <div class="monuments">
       <a href="architecture/MediciRiccardi.php"><img class="image3" src="../../Images/MediciRiccardi/Palazzo_medici_riccardi.JPG" alt="MediciRiccardi"></a>
-      <h3>Medici Riccardi</h3>
+      <h3><a class="titreMonument" href="architecture/MediciRiccardi.php">Medici Riccardi</a></h3>
     </div>
 
   </div>

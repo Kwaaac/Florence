45fbd6ec55c2567737f054bcd56ab9f572fbd10e
Schneider <leoschneidermaunoury@gmail.com>
2019-06-06 20:12:54 +0200

    Test inscription

--- fr/Pages/Sousmenus/inscription.php
+++ fr/Pages/Sousmenus/inscription.php
@@ -1,18 +1,4 @@
-<?php
-if(isset($_POST["Mdp"])){
-  if($_POST["Mdp"]== $_POST["Mdp2"]){
-    $check->query("SELECT * FROM USER WHERE login=\"".$_POST["login"]."\";")
-    if ($check->rowCount()!=0){
-      $dbh->query("INSERT INTO USER VALUES (\"".$_POST["login"].\"\", SHA1(\"".$_POST["Mdp"]."\"));");
-    }
-    else {
-      $error2=true;
-    }
-  }else{
-    $error1=true;
-  }
-}
-?>
+
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -20,23 +6,7 @@ if(isset($_POST["Mdp"])){
     <title>[Florence] Inscription</title>
   </head>
   <body id="inscription">
-    <?php include('../../HeadAndFoot/headerStg2.php');
-    if (isset($error1)) {
-      ?>
-      <div class="alert alert-primary">
-        <p>Les Mots De passe ne correspondent pas.</p>
-      </div>
-      <?php
-    }
-
-    if (isset($error2)) {
-      ?>
-      <div class="alert alert-primary">
-        <p>Cet identifiant n'est plus disponible.</p>
-      </div>
-      <?php
-    }
-    ?>
+    <?php include('../../HeadAndFoot/headerStg2.php');?>
     <form class="" method="post">
       <div class="form-group">
         <div class="form-row">

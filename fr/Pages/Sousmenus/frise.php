047a35e937aeb673869dc78fd5d808cfad92fe7d
Schneider <leoschneidermaunoury@gmail.com>
2019-06-06 16:23:09 +0200

    Ajout frise JS 2eme edition (la premiere a pas marche??)

--- fr/Pages/Sousmenus/frise.php
+++ fr/Pages/Sousmenus/frise.php
@@ -14,11 +14,8 @@
     <span> Frise chronologique de Florence</span>
   </div>
 
-  <div class="frise">
 
-    <img src="../../Images/frise_chrono.png" alt="Frise Chronologique">
-
-  </div>
+  <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1pMEyAqytbrtjm4T7MTgozCIpgbhjppJ4W03qWOKii3A&font=Default&lang=en&initial_zoom=2&height=800' width='100%' height='800' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
 
   <?php include('../../HeadAndFoot/footerStg2.php'); ?>
 </body>
